94efee9205ead3d50ae79185b8d5e724c6ed0394
aleattene <attene.tomorrowdevs@gmail.com>
2025-01-30 10:34:10 +0100

    feat: add payment and sport-certificate models

---
+++ documentation/models/sport_certificates.py
@@ -0,0 +1,51 @@
+from django.db import models
+from profiles.models.sport_doctors import SportDoctor
+from profiles.models.athletes import Athlete
+
+
+class SportCertificate(models.Model):
+    # facility_id = models.CharField(
+    #     max_length=50, verbose_name="Identificativo Struttura Erogatrice"
+    # )
+    issue_date = models.DateField(verbose_name="Data di Emissione")
+    expiration_date = models.DateField(verbose_name="Data di Scadenza")
+
+    athlete = models.ForeignKey(
+        Athlete,
+        on_delete=models.PROTECT,
+        verbose_name="Atleta",
+        related_name="medical_certificates"
+    )
+
+    sport_doctor = models.ForeignKey(
+        SportDoctor,
+        on_delete=models.PROTECT,
+        verbose_name="Medico",
+        related_name="certificates"
+    )
+
+    class Meta:
+        verbose_name = "Certificato Medico Sportivo"
+        verbose_name_plural = "Certificati Medico Sportivi"
+        ordering = ["-issued_date"]
+
+    def __str__(self):
+        return f"Certificato emesso il {self.issued_date} da {self.doctor} per {self.athlete}"
+
+    def __repr__(self):
+        return (f"SportCertificate, issued_date={self.issued_date}, "
+                f"expiration_date={self.expiration_date}, doctor={self.doctor}),"
+                f"athlete={self.athlete}")
+
+
+
+# SQL code provided by @DaveAttene
+#  CREATE TABLE CertificazioniSanitarie (
+#    id_certificazione INT PRIMARY KEY,
+#    data_emissione DATE NOT NULL,
+#    data_scadenza DATE NOT NULL,
+#    id_atleta INT NOT NULL,
+#    id_medico INT NOT NULL,
+#    FOREIGN KEY (id_atleta) REFERENCES Atleti (id_atleta),
+#    FOREIGN KEY (id_medico) REFERENCES Medici (id_medico)
+#  );
