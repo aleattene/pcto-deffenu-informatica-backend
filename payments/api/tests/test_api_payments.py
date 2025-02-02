802c65a9dd190b7e2c466a003aadbdfe778420d9
aleattene <attene.tomorrowdevs@gmail.com>
2025-02-02 11:18:44 +0100

    test(payments): add test payments crud api

--- payments/api/tests/test_api_payments.py
+++ payments/api/tests/test_api_payments.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 from payments.models.payments import Payment
@@ -21,23 +22,26 @@ class PaymentAPITestCase(TestCase):
         self.payment_data = {
             "payment_date": "2024-02-01",
             "amount": "1000.00",
-            "trainer": self.trainer.id
+            "trainer": self.trainer
         }
 
         self.payment = Payment.objects.create(**self.payment_data)
-        self.payment_url = f"/payments/{self.payment.id}/"
+        self.payment_list_url = reverse("payments:payments-list")
+        self.payment_detail_url = f"{self.payment_list_url}{self.payment.id}/"
 
     # Read Test: GET all payments
     def test_get_all_payments(self):
-        response = self.client.get("/payments/")
+        response = self.client.get(self.payment_detail_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertGreater(len(response.data), 0)
 
     # Read Test: GET single payment
     def test_get_single_payment(self):
-        response = self.client.get(self.payment_url)
+        response = self.client.get(self.payment_detail_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["payment_date"], "2024-02-01")
         self.assertEqual(response.data["amount"], "1000.00")
+        self.assertEqual(response.data["trainer"], self.trainer.id)
 
     # Create Test: POST new payment
     def test_create_payment(self):
@@ -46,28 +50,36 @@ class PaymentAPITestCase(TestCase):
             "amount": "1500.00",
             "trainer": self.trainer.id
         }
-        response = self.client.post("/payments/", new_payment, format="json")
+        response = self.client.post(self.payment_list_url, new_payment, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Payment.objects.count(), 2)
 
     # Update Test: PUT payment
     def test_update_payment(self):
         updated_data = self.payment_data.copy()
+        updated_data["payment_date"] = "2024-04-01"
         updated_data["amount"] = "1200.00"
-        response = self.client.put(self.payment_url, updated_data, format="json")
+        updated_data["trainer"] = self.trainer.id
+        response = self.client.put(self.payment_detail_url, updated_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.payment.refresh_from_db()
+        self.assertEqual(str(self.payment.payment_date), "2024-04-01")
         self.assertEqual(str(self.payment.amount), "1200.00")
+        self.assertEqual(self.payment.trainer.id, self.trainer.id)
 
     # Update Test: PATCH payment
     def test_partial_update_payment(self):
-        response = self.client.patch(self.payment_url, {"amount": "1800.00"}, format="json")
+        updated_data = self.payment_data.copy()
+        updated_data["payment_date"] = "2023-01-31"
+        updated_data["trainer"] = self.trainer.id
+        response = self.client.patch(self.payment_detail_url, updated_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.payment.refresh_from_db()
-        self.assertEqual(str(self.payment.amount), "1800.00")
+        self.assertEqual(str(self.payment.payment_date), "2023-01-31")
+        self.assertEqual(self.payment.trainer.id, self.trainer.id)
 
     # Delete Test: DELETE payment
     def test_delete_payment(self):
-        response = self.client.delete(self.payment_url)
+        response = self.client.delete(self.payment_detail_url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Payment.objects.count(), 0)
