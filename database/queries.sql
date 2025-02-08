dfacc7c777fdb9f79072db0b2477170a7ca5a14e
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2025-02-08 19:45:25 +0100

    chore: improve code style

--- database/queries.sql
+++ database/queries.sql
@@ -1,92 +1,109 @@
-/* athlets read */
+/* ATHLETES CRUD queries */
+
+/* athletes Read */
 SELECT * 
 FROM  profiles_athlete;
 
-/* athlets create */
+/* athletes Create */
 INSERT INTO profiles_athlete (first_name, last_name, date_of_birth, place_of_birth, fiscal_code, category_id)
 VALUES ('Luca', 'Sanna', '2000-01-01', 'Terni', 'ADMSJFKCNH101094', 2)
 
-/* athlets delete */
+/* athletes Delete */
 DELETE FROM profiles_athlete 
 WHERE id=3;
 
-/* athlets update */
+/* athletes Update */
 UPDATE profiles_athlete 
 SET place_of_birth = 'Torino'
 WHERE id=6;
 
+===========================
 
-/* trainers read */
+/* TRAINERS CRUD queries */
+
+/* trainers Read */
 SELECT *
 FROM profiles_trainer
   
-/* trainers create */
+/* trainers Create */
 INSERT INTO profiles_trainer (first_name, last_name, fiscal_code)
 VALUES ('Davide', 'Rossi', 'ADMSJFKCNH901094')
 
-/* trainers delete */
+/* trainers Delete */
 DELETE FROM profiles_trainer
 WHERE id = 3;
 
-/* trainers update */
+/* trainers Update */
 UPDATE profiles_trainer
 SET fiscal_code = 'RBTTYE55G10K987W'
 WHERE id=1;
 
+================================
 
-/* sport-doctors read */
+/* SPORT DOCTORS CRUD queries */
+
+/* sport-doctors Read */
 SELECT *
 FROM profiles_sportdoctor
 
-/* sport-doctors create */
+/* sport-doctors Create */
 INSERT INTO profiles_sportdoctor (first_name, last_name, vat_number)
 VALUES ('Riccardo', 'Piro', '12345678912')
 
-/* sport-doctors delete */
+/* sport-doctors Delete */
 DELETE FROM profiles_sportdoctor
 WHERE id = 1;
 
-/* sport-doctors update */
+/* sport-doctors Update */
 UPDATE profiles_sportdoctor
 SET last_name = 'Tornaindietro'
 WHERE id=2;
 
+===========================
 
-/* payments read */
+/* PAYMENTS CRUD queries */
+
+/* payments Read */
 SELECT *
 FROM payments_payment
 
-/* payments create */
+/* payments Create */
 INSERT INTO payments_payment (payment_date, amount, trainer_id)
 VALUES ('2024-10-01', 500, 2)
 
-/* payments delete */
+/* payments Delete */
 DELETE FROM payments_payment
 WHERE id = 3;
 
-/* payments update */
+/* payments Update */
 UPDATE payments_payment
 SET amount = 1500
 WHERE id=2;
 
+=====================================
 
-/* sport-certificates read */
+/* SPORT CERTIFICATES CRUD queries */
+
+/* sport-certificates Read */
 SELECT *
 FROM documentation_sportcertificate
 
-/* sport-certificates create */
+/* sport-certificates Create */
 INSERT INTO documentation_sportcertificate (issue_date, expiration_date, athlete_id, sport_doctor_id)
 VALUES ('2024-12-05', '2025-12-05', 1, 3)
 
-/* sport-certificates delete */
+/* sport-certificates Delete */
 DELETE FROM documentation_sportcertificate
 WHERE id = 1;
 
-/* sport-certificates update */
+/* sport-certificates Update */
 UPDATE documentation_sportcertificate
 SET issue_date = '2024-11-05', expiration_date = '2025-11-05'
 WHERE id=2;
 
+==============================
+
+/* ATHLETE - CATEGORY query */
 
 /* athletes-category-AF */
 SELECT first_name, last_name
@@ -95,7 +112,7 @@ JOIN profiles_category AS pc
 ON pa.category_id = pc.id
 WHERE pc.code = 'AF';
 
-
+==============================
 
 
 
