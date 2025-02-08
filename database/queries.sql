5c7bdbb37351d28d336de8a8307917b70ed77ed9
Davide Attene <attene.davide.git@gmail.com>
2025-02-08 18:47:19 +0100

    feat: add sport-doctors crud queries

--- database/queries.sql
+++ database/queries.sql
@@ -34,6 +34,22 @@ SET fiscal_code = 'RBTTYE55G10K987W'
 WHERE id=1;
 
 
+/* sport-doctors read */
+SELECT *
+FROM profiles_sportdoctor
+
+/* sport-doctors create */
+INSERT INTO profiles_sportdoctor (first_name, last_name, vat_number)
+VALUES ('Riccardo', 'Piro', '12345678912')
+
+/* sport-doctors delete */
+DELETE FROM profiles_sportdoctor
+WHERE id = 1;
+
+/* sport-doctors update */
+UPDATE profiles_sportdoctor
+SET last_name = 'Tornaindietro'
+WHERE id=2;
 
 
 
