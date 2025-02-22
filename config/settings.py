00717ec2afcc12382a0ba9ac235c507922c22207
aleattene <attene.tomorrowdevs@gmail.com>
2025-02-22 15:23:39 +0100

    fix: add production cors path

--- config/settings.py
+++ config/settings.py
@@ -31,9 +31,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://pcto-deffenu.netlify.app"
 ]
 
 
